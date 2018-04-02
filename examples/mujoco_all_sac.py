13a51dddd0adb43ed8fce92c85706e78083734b6
Aurick Zhou <azhou42@berkeley.edu>
2018-04-02 00:43:25 -0700

    added docker image

--- examples/mujoco_all_sac.py
+++ examples/mujoco_all_sac.py
@@ -2,6 +2,7 @@ import argparse
 
 from rllab.envs.normalized_env import normalize
 from rllab.misc.instrument import VariantGenerator
+from rllab import config
 
 from sac.algos import SAC
 from sac.envs.gym_env import GymEnv
@@ -11,15 +12,17 @@ from sac.policies.gmm import GMMPolicy
 from sac.replay_buffers import SimpleReplayBuffer
 from sac.value_functions import NNQFunction, NNVFunction
 
+config.DOCKER_IMAGE = "haarnoja/rllab"  # needs psutils
+config.AWS_IMAGE_ID = "ami-a3a8b3da"  # with docker already pulled
 
 COMMON_PARAMS = {
-    "seed": [1, 2, 3],
-    "lr": 3E-4,
+    "seed": [1, 11, 21],
+    "lr": [1E-4, 3E-4],
     "discount": 0.99,
     "tau": 0.01,
-    "K": 4,
-    "layer_size": 128,
-    "batch_size": 128,
+    "K": 1,
+    "layer_size": 256,
+    "batch_size": 256,
     "max_pool_size": 1E6,
     "n_train_repeat": 1,
     "epoch_length": 1000,
@@ -64,7 +67,7 @@ ENV_PARAMS = {
         'env_name': 'Ant-v1',
         'max_path_length': 1000,
         'n_epochs': 10000,
-        'scale_reward': 3,
+        'scale_reward': [3,5,10],
     },
     'humanoid': { # 21 DoF
         'prefix': 'humanoid',
